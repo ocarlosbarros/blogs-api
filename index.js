ce10ceacfd101b4d739324ba1626eaada83ba606
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-06 23:05:36 -0300

    feat:apply use to Router and express.json middlewares

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const Router = require('./routes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
+app.use(Router);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
