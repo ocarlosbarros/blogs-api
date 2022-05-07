5b157da42b8c60e801773c74052d7382c9055bfa
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-06 22:08:54 -0300

    feat:create model PostCategories with association

---
+++ models/PostCategories.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostsCategories = sequelize.define('PostsCategory',
+    {},
+    { timestamps: false });
+
+    PostsCategories.associate = (models) => {
+    models.BlogPost.hasMany(models.Category, {
+      as: 'categories',
+      through: PostsCategories,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostsCategories,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+  return PostsCategories;
+};
