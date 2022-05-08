7e23c5755d68dc1f17b6687fc3a9a2ea98c67519
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-08 16:41:52 -0300

    refactor:refactoring of errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,22 +1,24 @@
 const Joi = require('joi');
 
 const errorHandler = (error, _request, response, _next) => {
-    if (error.message === 'Validation error') {
-    return response.status(409)
-                    .json({ message: 'User already registered' }); 
-}
-
-    if (error.message === 'Invalid fields') {
-    return response.status(400)
-                    .json({ message: error.message }); 
-}
-    
     if (Joi.isError(error)) return response.status(400).json({ message: error.message });
+    
+    switch (error.message) {
+        case 'Validation error':
+        return response.status(409).json({ message: 'User already registered' }); 
+        
+        case 'Invalid fields':
+        return response.status(400).json({ message: error.message }); 
+        
+        case 'jwt malformed':
+        return response.status(401).json({ message: 'Expired or invalid token' }); 
 
-    // error log to developers
-    console.error(error);
-
-    return response.status(500).json({ message: 'Oops, something\'s wrong! Contact our support!' });
+        default:
+        // error log to developers
+        console.error(error);
+       return response.status(500)
+                        .json({ message: 'Oops, something\'s wrong! Contact our support!' });
+    }
 };
 
 module.exports = errorHandler;
