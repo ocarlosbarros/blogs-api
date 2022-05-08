0a226f4ffe17909602b79c58830afbd687434ce8
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-08 16:40:53 -0300

    feat:adds EnsureAuthenticated to valid token

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,9 @@
 const UserRouter = require('express').Router();
 const UserController = require('../controllers/UserController');
 const ValidateUser = require('../middleware/validateUserMiddleware');
+const EnsureAuthenticated = require('../middleware/ensureAuthenticated');
 
 UserRouter.post('/', ValidateUser, UserController.create);
+UserRouter.get('/', EnsureAuthenticated, UserController.findAll);
 
 module.exports = UserRouter;
