ec9a32e6ddc6fcce1551e350ecca280371e542c7
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-13 10:50:03 -0300

    feat:add route to update a post

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,10 +1,11 @@
 const PostRouter = require('express').Router();
 const PostController = require('../controllers/PostController');
 const EnsureAuthenticated = require('../middleware/ensureAuthenticated');
-const ValidatePost = require('../middleware/validatePostMiddleware');
+const Validate = require('../middleware/validatePostMiddleware');
 
-PostRouter.post('/', EnsureAuthenticated, ValidatePost, PostController.create);
+PostRouter.post('/', EnsureAuthenticated, Validate.createBlogPost, PostController.create);
 PostRouter.get('/', EnsureAuthenticated, PostController.findAll);
 PostRouter.get('/:id', EnsureAuthenticated, PostController.findById);
+PostRouter.put('/:id', EnsureAuthenticated, Validate.updateBlogPost, PostController.update);
 
 module.exports = PostRouter;
