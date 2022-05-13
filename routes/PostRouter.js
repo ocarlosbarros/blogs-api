cc0e315bddc24a8299617ce9cfe50ade9a627b73
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-13 11:22:56 -0300

    refactor: fix lint and add EnsureAuthorized middleware

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -1,11 +1,13 @@
 const PostRouter = require('express').Router();
 const PostController = require('../controllers/PostController');
 const EnsureAuthenticated = require('../middleware/ensureAuthenticated');
+const EnsureAuthorized = require('../middleware/ensureAuthorized');
 const Validate = require('../middleware/validatePostMiddleware');
 
 PostRouter.post('/', EnsureAuthenticated, Validate.createBlogPost, PostController.create);
 PostRouter.get('/', EnsureAuthenticated, PostController.findAll);
 PostRouter.get('/:id', EnsureAuthenticated, PostController.findById);
-PostRouter.put('/:id', EnsureAuthenticated, Validate.updateBlogPost, PostController.update);
+PostRouter.put('/:id', EnsureAuthenticated, EnsureAuthorized, 
+Validate.updateBlogPost, PostController.update);
 
 module.exports = PostRouter;
