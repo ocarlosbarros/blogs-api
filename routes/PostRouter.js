d8d830f103edc59f32353852a874f130cb9724c1
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-13 09:10:31 -0300

    feat:adds route to get post by id

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -5,5 +5,6 @@ const ValidatePost = require('../middleware/validatePostMiddleware');
 
 PostRouter.post('/', EnsureAuthenticated, ValidatePost, PostController.create);
 PostRouter.get('/', EnsureAuthenticated, PostController.findAll);
+PostRouter.get('/:id', EnsureAuthenticated, PostController.findById);
 
 module.exports = PostRouter;
