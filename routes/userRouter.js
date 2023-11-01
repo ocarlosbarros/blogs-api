451b5324b292589df0e70f1bf89c12961593f1aa
Carlos Barros <carlos_dbs@hotmail.com>
2023-11-01 15:04:39 -0300

    feat(userRouter):adds delete

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,6 @@ const EnsureAuthenticated = require('../middleware/ensureAuthenticated');
 UserRouter.post('/', ValidateUser, UserController.create);
 UserRouter.get('/', EnsureAuthenticated, UserController.findAll);
 UserRouter.get('/:id', EnsureAuthenticated, UserController.findById);
+UserRouter.delete('/me', EnsureAuthenticated, UserController.destroy);
 
 module.exports = UserRouter;
