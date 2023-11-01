ddd0637f22ee1f52596a1d50a9ec5b241cfd6464
Carlos Barros <carlos_dbs@hotmail.com>
2023-11-01 15:05:45 -0300

    feat(BlogPost):changes the model

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,10 +1,21 @@
+const User = require('./User');
+
+const userReference = (DataTypes) => ({
+        type: DataTypes.INTEGER, 
+            allowNull: false,
+            foreignKey: true,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE',
+            references: { model: User, key: 'userId' },
+    });
+
 module.exports = (sequelize, DataTypes) => {
     const BlogPost = sequelize.define('BlogPost', {
         title: DataTypes.STRING,
         content: DataTypes.STRING,
         published: DataTypes.DATE,
         updated: DataTypes.DATE,
-        userId: { type: DataTypes.INTEGER, foreignKey: true }, // optional
+        userId: userReference(DataTypes), // optional
     }, { timestamps: false });
 
     BlogPost.associate = (models) => {
